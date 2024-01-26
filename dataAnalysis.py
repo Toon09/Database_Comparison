fbd01b52d8a7802f14ffd490b0840583e85ee07c
Raul Boekhoudt <sirapex4@gmail.com>
2024-01-26 01:07:38 +0100

    instructions

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,3 +1,6 @@
 import csv
 import matplotlib
 
+# make graphs
+# make folders for data with compressor alg and without
+# run another piece of code (folder_name) instead of changing the code for MongoDB 
